09b3cc4308fd1a27652990521dc2fa8b1323fc8e
Ayush Maiti <ayush.maiti39@gmail.com>
2025-12-24 00:03:27 +0530

    drills

--- drills.py
+++ drills.py
@@ -5,3 +5,15 @@ class User:
         self.followers = 0
         self.following = 0
         pass
+    def follow(self, user):
+        self.following += 1
+        user.followers += 1
+
+
+user_1 = User("001", "Ayush")
+user_2 = User("002", "Jack")
+
+user_1.follow(user_2)
+
+print(user_1.following) # Should be 1
+print(user_2.followers) # Should be 1
